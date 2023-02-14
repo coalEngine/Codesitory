b5275efe70cb6fd938795ed531cc4e9bc3229226
Thiery <weirdogamingidk@gmail.com>
2023-02-14 10:31:19 -0500

    Refactored

--- Project_log/connect4.py
+++ Project_log/connect4.py
@@ -1,26 +1,62 @@
 from tkinter import *
 import tkinter as tk
 
-window = tk.Tk()
-window.title("Connect 4!")
+# Window Config
+window = Tk()
+window.title("Connect 4 (v1.0)")
+window.geometry("1200x800")
+window.config(bg="black")
+
+# Frame Config
+frame = Frame(window, bg="White")
+frame.pack(side=BOTTOM)
+
+# Game_Board Config
+player_1_token = ""
+player_2_token = ""
+board = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+
+# Main Text Config
+main_text = Label(
+    text="Connect 4",
+    font = ('Times 24 italic'),
+    bg="Black",
+    fg="White"
+    )
 
 
 # create buttons
 button_dict = {}
-for x in range(7):
-    for y in range(6):
+for y_pos in range(7):
+    for x_pos in range(6):
    #
    # def action(x = x): 
    #    return text_updation(x)   
    # create the buttons 
-        button_dict[x] = Button(window, text = '', width=5, height=2, bg="white")
-        button_dict[x].place(x=150*x, y=120*y)
+        button_dict[y_pos] = Button(
+            frame, 
+            text = '', 
+            width=16, 
+            height=7, 
+            bg="Black",
+         
+        )
+        button_dict[y_pos].grid(row=x_pos, column=y_pos)
+        
 
         
-d = 10
 def run():
+    main_text.pack()
     window.mainloop()
-
+    
+    
 run()
 
             
