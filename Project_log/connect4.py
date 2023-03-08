180f89f9d5fa721d89a607d5ea337192fd12db74
Thiery <weirdogamingidk@gmail.com>
2023-03-08 10:19:57 -0500

    Modded

--- Project_log/connect4.py
+++ Project_log/connect4.py
@@ -34,43 +34,34 @@ def drop_down():
 
 # create buttons
 
-button_dict = {}
-buttonClicked = False
+COORDS_LIST = []
+buttons_dict = {}
 
-for y_pos in range(7):
-    for x_pos in range(6):
-        def change_player(button_id):
-            p2 = True
-            player_select.config(text="Player_2_Turn")
-            button_dict[button_id].config(text='x')
-        
-     # create the buttons 
-        button_dict[y_pos] = Button(
-            frame, 
-            text = '', 
-            width=16, 
-            height=7, 
-            bg="Black",
-            command = lambda button_id=y_pos: change_player(button_id),
-            fg="Light Blue",
-        )
-        button_dict[y_pos].grid(row=x_pos, column=y_pos)
-        
+###########################################
+def fire_off(x, y):
+    print("column:{}, row:{}".format(x, y))
+    newValue = 0
+    value = '{}_{}'.format(x, y)
 
+###########################################
+
+for r in range(6):
+    for c in range(7):
+        coord = str(r)+"_"+str(c)
+        COORDS_LIST.append(coord)
+        buttons_dict[COORDS_LIST[-1]] = Button(frame, text="O", width="16", height="7", bg="Black", fg="White")
+        ###########################################################################
+        buttons_dict[COORDS_LIST[-1]]["command"] = lambda x=c, y=r : fire_off(x, y)
+        ###########################################################################
+        buttons_dict[COORDS_LIST[-1]].grid(row=r,column=c)
+
+print(COORDS_LIST)
         
 def run():
     main_text.place(x=536, y=0)
     player_select.place(x=0, y=0)
     window.mainloop()
 
-board_data = [
-        [frame.children["!button"], frame.children["!button2"], frame.children["!button3"], frame.children["!button4"], frame.children["!button5"], frame.children["!button6"], frame.children["!button7"]],
-        [frame.children["!button8"], frame.children["!button9"], frame.children["!button10"], frame.children["!button11"], frame.children["!button12"], frame.children["!button13"], frame.children["!button14"]],
-        [frame.children["!button15"], frame.children["!button16"], frame.children["!button17"], frame.children["!button18"], frame.children["!button19"], frame.children["!button20"], frame.children["!button21"]],
-        [frame.children["!button22"], frame.children["!button23"], frame.children["!button24"], frame.children["!button25"], frame.children["!button26"], frame.children["!button27"], frame.children["!button28"]],
-        [frame.children["!button29"], frame.children["!button30"], frame.children["!button31"], frame.children["!button32"], frame.children["!button33"], frame.children["!button34"], frame.children["!button35"]],
-        [frame.children["!button36"], frame.children["!button37"], frame.children["!button38"], frame.children["!button39"], frame.children["!button40"], frame.children["!button41"], frame.children["!button42"]]
-]
 
 while not game_over:
     run()
