c86b63953c1fecb3fd5f9ba4a607e1f5850b4dc9
Thiery <weirdogamingidk@gmail.com>
2023-03-16 10:20:40 -0400

    Modded

---
+++ Notes/compare.py
@@ -0,0 +1,12 @@
+nil, num = 0, 0
+max = 1
+cap = 'A'
+low = 'a'
+
+print("Equality :\t", nil, '==', num, nil==num)
+print("Equality :\t", cap, '==', low, cap==low)
+print("Inequality :\t", nil, '!=', num, num!=nil)
+print("Greater :\t", nil, '>', max, nil>max)
+print("Lesser :\t", nil, '<', max, nil<max)
+print("More or Equal :\t", nil, '>=', num, nil>=num)
+print("More or Equal :\t", nil, '<=', num, nil<=num)
