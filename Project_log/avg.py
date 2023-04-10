7339b3026d87ebf1a43268afbc908eb2594923d2
Thiery <weirdogamingidk@gmail.com>
2023-04-10 08:33:22 -0400

    new files

---
+++ Project_log/avg.py
@@ -0,0 +1,9 @@
+avg = 0
+rangee = 10
+for _ in range(rangee):
+    ask = float(input("Number: "))
+    avg += ask
+print("/nThe Average is", avg / rangee)
+
+
+    
---
+++ Project_log/minmax.py
@@ -0,0 +1,6 @@
+nums = []
+for _ in range(10):
+    ask = int(input("Number?: "))
+    nums.append(ask)
+print("\nThe maximum value is", max(nums),"\n")
+print("The minimum value is", min(nums))
