3629d07669a3c1d8a9426a892c886f666c2ed0ff
Thiery <weirdogamingidk@gmail.com>
2023-04-13 10:34:14 -0400

    mod

--- Project_log/list.py
+++ Project_log/list.py
@@ -1,5 +1,28 @@
+import sys
 form = []
 for _ in range(10):
     nums = ['O']*10
     form.append(nums)
-print(form)
+def print_list():
+    print("[{},".format(form[0]))
+    print(" {},".format(form[1]))
+    print(" {},".format(form[2]))
+    print(" {},".format(form[3]))
+    print(" {},".format(form[4]))
+    print(" {},".format(form[5]))
+    print(" {},".format(form[6]))
+    print(" {},".format(form[7]))
+    print(" {},".format(form[8]))
+    print(" {}]".format(form[9]))
+
+def change_list(posX, posY):
+    form[posX][posY] = 'X'
+    print_list()
+
+times = int(input("How many times do you want to change the list?: "))
+for _ in range(times):
+    selection_1 = int(input("Enter in a row: "))
+    selection_2 = int(input("Enter in a column: "))
+    change_list(selection_1, selection_2)
+
+
