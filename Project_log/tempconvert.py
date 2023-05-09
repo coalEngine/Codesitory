45ce5be4cb2a0b8861c034d16c84d1d060dc9034
Thiery <weirdogamingidk@gmail.com>
2023-05-09 10:35:08 -0400

    modded

--- Project_log/tempconvert.py
+++ Project_log/tempconvert.py
@@ -6,6 +6,8 @@ def convert(choice, choice2):
             print("Rankine: ", F + 459.7)
         elif choice2 == 'C':
             print("Celsius: ", (F - 32) / 1.8)
+        elif choice2 == "K":
+            print("Kelvin: ", (F- 459.67) / (5/9))
         else:
             print("Invalid")
     if choice == 'C':
@@ -14,8 +16,19 @@ def convert(choice, choice2):
              print("Fahrenheit: ", (1.8 * C) + 32)
          if choice2 == 'K':
              print("Kelvin: ", C + 273.2)
+         if choice2 == "R":
+            print("Rankine: ", (C + 273.15) * 1.8)
     if choice == 'R':
-        pass
+        R = float(input("Please input the Rankine: "))
+        if choice2 == 'F':
+            print("Fahreheit: ",  R - 459.67)
+        if choice2 == 'K':
+            print("Kelvin: ", R / 1.8)
+        if choice2 == 'C':
+            print("Celsius: ", (R / (9/5)) - 491.67)
+    if choice == 'K':
+         R = float(input("Please input the Rankine: "))
+         
 def intro():
     choice = input("What do you want to convert?: ")
     choice2 = input("To?: ")
