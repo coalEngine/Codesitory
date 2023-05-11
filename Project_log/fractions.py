2780ec07f3b7675b4981947b0d997c171895c2f8
Thiery <weirdogamingidk@gmail.com>
2023-05-11 10:25:30 -0400

    modded

--- Project_log/fractions.py
+++ Project_log/fractions.py
@@ -1,4 +1,18 @@
 from fraction import *
+from math import *
+
+def reduceFraction(x, y) :
+     
+    d = gcd(x, y);
+ 
+    x = x // d;
+    y = y // d;
+    count = 0
+    if x > y:
+       count+=1
+       print(count)
+    else:
+        print(f"{x}/{y}")
 
 while True:
     print("Type Q to Quit")
@@ -12,5 +26,9 @@ while True:
     elif fraction2_piece2 == 0:
         print("fraction2.piece2 is undefined")
         break
-    final = Fraction(fraction1_piece1, fraction2_piece2) * Fraction(fraction2_piece1, fraction2_piece2)
-    print(f"{fraction1_piece1}/{fraction1_piece2} * {fraction2_piece1}/{fraction2_piece2} = {final}")
+    final = fraction1_piece1 * fraction2_piece1 
+    final2 = fraction1_piece2 * fraction2_piece2
+
+    
+    print(f"{fraction1_piece1}/{fraction1_piece2} * {fraction2_piece1}/{fraction2_piece2} = ", end="")
+    reduceFraction(final, final2)
