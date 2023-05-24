fc99507b52f1a5b4b23c17d7d1b71be1b8ebd5d6
Thiery <weirdogamingidk@gmail.com>
2023-05-24 10:31:27 -0400

    d

--- Project_log/prime.py
+++ Project_log/prime.py
@@ -1,24 +1,15 @@
-num = int(input("Number?: "))
+# Python program to display all the prime numbers within an interval
 
-# To take input from the user
-#num = int(input("Enter a number: "))
+lower = int(input("Lower Bound: "))
+upper = int(input("Higher Bound: "))
 
-# define a flag variable
-flag = False
+print("Prime numbers between", lower, "and", upper, "are:")
 
-if num == 1:
-    print(num, "is not a prime number")
-elif num > 1:
-    # check for factors
-    for i in range(2, num):
-        if (num % i) == 0:
-            # if factor is found, set flag to True
-            flag = True
-            # break out of loop
-            break
-
-    # check if flag is True
-    if flag:
-        print(num, "is not a prime number")
-    else:
-        print(num, "is a prime number")
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
