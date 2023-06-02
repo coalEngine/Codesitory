d89090a97d4dbf859a7313cf21e7b9bc83bccd24
Thiery <weirdogamingidk@gmail.com>
2023-06-02 10:30:38 -0400

    n

--- Notes/time.py
+++ Notes/time.py
@@ -2,6 +2,12 @@ from datetime import *
 today = datetime.today()
 print("Today is:", today)
 
-for attri in today:
-    ['year','month','day','hour','minute','second','microsecond']
+for attri in \
+['year','month','day','hour','minute','second','microsecond']:
     print(attri, ':\t', getattr(today, attri))
+
+print('Time: ', today.hour, ':', today.minute, sep="")
+day = today.strftime('%A')
+month = today.strftime('%B')
+
+print("Date:", day, month, today.day)
---
+++ Notes/timer.py
@@ -0,0 +1,14 @@
+from time import *
+start_timer = time()
+struct = localtime(start_timer)
+print("\nStarting countdown at:", strftime('%X', struct))
+
+i = 10
+while i > -1:
+    print(i)
+    i -= 1
+    sleep(1)
+
+end_time = time()
+diff = round(end_time - start_timer)
+print("Runtime", diff, 'secs')
